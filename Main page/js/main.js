9e2433e1ebe9dcd31444847ef3a5de201755c4cb
Wissem-Zidi <wissem.zidi.ofc@gmail.com>
2022-07-28 04:25:16 +0100

    finish the conent of the page__ footer not doed

--- Main page/js/main.js
+++ Main page/js/main.js
@@ -1,7 +1,72 @@
 function afficheBox1() {
   document.getElementById('list1').style.display = "block"
+  document.getElementById('list1').style.height = "fit-content"
+  document.getElementById('list1').style.animation = "fade_in 0.3s ease-out forwards"
+  console.log("showing Box1...")
 }
 
 function hideBox1() {
   document.getElementById('list1').style.display = "none"
+  console.log("hiding Box1...")
 }
+
+function afficheBox2() {
+  document.getElementById('list2').style.display = "block"
+  document.getElementById('list2').style.height = "fit-content"
+  document.getElementById('list2').style.animation = "fade_in 0.3s ease-out forwards"
+  console.log("showing Box1...")
+}
+
+function hideBox2() {
+  document.getElementById('list2').style.display = "none"
+  console.log("hiding Box2...")
+}
+
+function afficheBox3() {
+  document.getElementById('list3').style.display = "block"
+  document.getElementById('list3').style.height = "fit-content"
+  document.getElementById('list3').style.animation = "fade_in 0.3s ease-out forwards"
+  console.log("showing Box1...")
+}
+
+function hideBox3() {
+  document.getElementById('list3').style.display = "none"
+  console.log("hiding Box3...")
+}
+
+function afficheBox4() {
+  document.getElementById('list4').style.display = "block"
+  document.getElementById('list4').style.height = "fit-content"
+  document.getElementById('list4').style.animation = "fade_in 0.3s ease-out forwards"
+  console.log("showing Box1...")
+}
+
+function hideBox4() {
+  document.getElementById('list4').style.display = "none"
+  console.log("hiding Box3...")
+}
+
+function afficheBox5() {
+  document.getElementById('list5').style.display = "block"
+  document.getElementById('list5').style.height = "fit-content"
+  document.getElementById('list5').style.animation = "fade_in 0.3s ease-out forwards"
+  console.log("showing Box1...")
+}
+
+function hideBox5() {
+  document.getElementById('list5').style.display = "none"
+  console.log("hiding Box3...")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("p2h").style.borderColor = 'red'
